3424f5741f163c5e04f57496387b1dd7d63bef75
marquinhos03 <mliguempibozzano@gmail.com>
2025-05-02 17:54:13 -0400

    Documentación + mod Monedas.java

--- src/main/java/org/example/Moneda.java
+++ src/main/java/org/example/Moneda.java
@@ -1,16 +1,45 @@
 package org.example;
 
-abstract class Moneda {
+/**
+ * Clase abstracta que representa una moneda.
+ */
+public abstract class Moneda implements Comparable<Moneda> {
     private static int contadorSerie = 0;
-    private int serie;
+    private final int serie;
 
+    /**
+     * Constructor de la moneda, asigna un número de serie único.
+     */
     public Moneda() {
         this.serie = contadorSerie++;
     }
 
+    /**
+     * Devuelve el número de serie de la moneda.
+     * @return número de serie.
+     */
     public int getSerie() {
         return serie;
     }
 
+    /**
+     * Devuelve el valor de la moneda.
+     * @return valor en pesos.
+     */
     public abstract int getValor();
+
+    /**
+     * Implementación de compareTo para ordenar por valor de menor a mayor.
+     * @param otra Otra moneda con la que comparar.
+     * @return -1 si es menor, 0 si igual, 1 si mayor.
+     */
+    @Override
+    public int compareTo(Moneda otra) {
+        return Integer.compare(this.getValor(), otra.getValor());
+    }
+
+    @Override
+    public String toString() {
+        return "$" + getValor() + " (serie " + serie + ")";
+    }
 }
